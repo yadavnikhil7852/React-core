8b5224a9850091e54da141b95a8cb313c5ee1cee
Nikhil Yadav <nikhilyadav52168@gmail.com>
2026-01-22 17:47:53 +0530

    added condition rendering - new code

--- Conditional-Rendering/src/App.jsx
+++ Conditional-Rendering/src/App.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import ComponentA from "./component/ComponentA";
+import ComponentB from "./component/ComponentB";
+import ComponentC from "./component/ComponentC";
 
+
+function StatusMessage({status}){
+    switch (status){
+    case "loading":
+      return <p>Loading...</p>
+      case 'success':
+        return <p>Result success</p> 
+        case 'error':
+        return <p>Error 404</p>
+     default :
+     return <p>Default h ye</p>
+  }}
+
+//function component for to do list
 function IsDone({ name, isChecked }) {
   //1st method
   // if (isChecked) {
@@ -24,61 +41,80 @@ function IsDone({ name, isChecked }) {
     </li>
   );
 }
-function Button({ children }) {
-  return (
-    <button type="button" className="ButtonDisplay">
-      {children}
-    </button>
-  );
+// function Button({ children }) {
+//   return (
+//     <button type="button" className="ButtonDisplay">
+//       {children}
+//     </button>
+//   );
+// }
+function ButtonDisplay({ name, isVisible }) {
+  if (isVisible) {
+    return <button className="ButtonDisplay">{name}</button>;
+  }
+  return <h1>Error </h1>;
 }
-
-function ButtonDisplay({name ,isVisible}){
-
-if(isVisible){
-  return <Button className="ButtonDisplay">{name}</Button>
-  
-}
-return <h1>Error </h1>
-
-}
-
 function App() {
-  const[isVisible , setIsVisible]= useState(false);
+  //usestate for button
+  const [isVisible, setIsVisible] = useState(false);
+  const options ='c';
+  let content = <h1>Hello how are you</h1>
+  if(options == 'a'){
+    content =<ComponentA/>
+  }
+  else if(options == 'b'){
+       content = <ComponentB/>
+  }
+  else  if(options =='c'){
+    content = <ComponentC/>
+  }
+  else{
+    content = <h1>component not displayed</h1>
+  }
+  //using switch case
+  
+  
   return (
     <>
       <h1>My to-do list</h1>
       <ul>
         <IsDone name="React 3 videos " isChecked={true} />
         <IsDone name="Code Review " isChecked={true} />
-        <IsDone name="Java DSA Explore " isChecked={false} />
+        <IsDone name="Java DSA Explore " isChecked={true} />
         <IsDone name="Reels + yt " isChecked={false} />
-{/* 
+        {/* 
        <ButtonDisplay name='show info' isVisible={true}/>
    <ButtonDisplay name='Hide info' isVisible={true}/> */}
-
       </ul>
-
-     <h3>Agar button pe click kiya toh aerohyre ka production udd jayega</h3>
-{
-  !isVisible && (
-    <button onClick={()=>{setIsVisible(true)}}>
-      mt kr lala mt kr🤯
-    </button>
-  ) 
-}
-{
-  isVisible && (
-    <button onClick={()=>{setIsVisible(false)}}>
-      bacha le jldi!
-    </button>
-  ) 
-}{
-  isVisible &&(
-    <p>le udd gya tera production , jaa raghav bhai ko bula pipe m tape mare</p>
-  )
-}
-   </>
+      <h3>Agar button pe click kiya toh aerohyre ka production udd jayega</h3>
+      {!isVisible && (
+        <button style={{backgroundColor : 'red'}}
+          onClick={() => {
+            setIsVisible(true); 
+          }}
+        >
+          mt kr lala mt kr🤯
+        </button>
+      )}
+      {isVisible && (
+        <button
+             style={{backgroundColor : 'green'}}
+          onClick={() => {
+            setIsVisible(false);
+          }}
+        >
+          bacha le jldi!
+        </button>
+      )}
+      {isVisible && (
+        <p>
+          le udd gya tera production , jaa raghav bhai ko bula pipe m tape mare
+        </p>
+      )}
+      <h3>{content}</h3> 
+      <StatusMessage status="loading" /> 
+    </>
+    
   );
 }
-
 export default App;
---
+++ Conditional-Rendering/src/var.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Vare() {
+  return (
+    <div>var</div>
+  )
+}
+
+export default Vare
